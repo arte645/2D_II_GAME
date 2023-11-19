f7c70946b4221f6952b13eadf46d0031305e20d6
pubupka <Halimovm111@gmail.com>
2023-11-19 17:41:04 +0600

    video 9
    теперь в одну ячейку нельзя ставить несколько башен сразу
    и иконка башни следует за курсором, после нажатия на кнопку выбора

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class TowerManager : Loader<TowerManager>
 {
-    TowerButton towerButtonPressed;
+    private TowerButton towerButtonPressed;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,7 +23,16 @@ public class TowerManager : Loader<TowerManager>
             RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero);
 
             if (hit.collider.tag == "TowerSide")
+            {
+                hit.collider.tag = "TowerSideFull";
+
                 PlaceTower(hit);
+            }
+        }
+
+        if (spriteRenderer.enabled)
+        {
+            FollowMouse();
         }
     }
 
@@ -32,12 +42,32 @@ public class TowerManager : Loader<TowerManager>
         {
             GameObject newTower = Instantiate(towerButtonPressed.TowerObject);
             newTower.transform.position = hit.transform.position;
+            DisableDrag();
         }
     }
 
     public void SelectedTower(TowerButton towerSelected)
     {
         towerButtonPressed = towerSelected;
+        EnableDrag(towerButtonPressed.DragSprite);
+
         Debug.Log("Pressed" + towerButtonPressed.gameObject);
+    }
+
+    public void FollowMouse()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    public void EnableDrag(Sprite sprite)  //М: хаха, drug
+    {
+        spriteRenderer.enabled = true;
+        spriteRenderer.sprite = sprite;
+    }
+
+    public void DisableDrag()
+    {
+        spriteRenderer.enabled = false;
     }
 }
