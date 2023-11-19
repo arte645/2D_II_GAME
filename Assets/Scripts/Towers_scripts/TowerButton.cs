e3e51bf866e24c074a8c1255f872cda220bf335b
pubupka <Halimovm111@gmail.com>
2023-11-19 16:22:27 +0600

    video 7 Добавлены кнопки выбора башен, запись в логи, о том, какую кнопку и когда нажали, текстуры 1-го и 2-го уровня электробашни, текстура кнопки выбора электробашни.

---
+++ Assets/Scripts/Towers_scripts/TowerButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject towerObject;
+
+    public GameObject TowerObject
+    {
+        get{
+            return towerObject;
+        }
+    }
+}
